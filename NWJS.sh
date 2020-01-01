ef6ceb631ee0cb3897d16a06a5d1dd69e30e74c9
WangTingZheng <2251985371@qq.com>
2020-01-01 13:59:15 +0800

    add install sh

---
+++ NWJS.sh
@@ -0,0 +1,1 @@
+/home/cpi/apps/nwjs-sdk-v0.27.6-linux-arm/nw /home/cpi/apps/gameshell-nwjs
---
+++ install.sh
@@ -0,0 +1,6 @@
+cd /home/cpi/apps/
+git clone https://github.com/WangTingZheng/gameshell-nwjs.git
+cd /home/cpi/launcher/Menu/GameShell/
+mkdir 98_NWJS
+cd 98_NWJS
+git clone https://github.com/WangTingZheng/gameshell-nwjs/NWJS.sh
